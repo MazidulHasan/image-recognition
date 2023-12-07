ff2924329f485b8fbcca06234745ed4e0b1e2de4
MazidulHasan <md.mazidulhasan1@gmail.com>
2023-12-08 01:47:12 +0600

    Updated

---
+++ app.py
@@ -0,0 +1,44 @@
+import pyrebase
+import zipfile
+import os
+from combineAll import print_all_digits
+
+config = {
+    "apiKey": "AIzaSyCw9nBF4EMtkM3FpK2ONtOdmkRFQjpp-3U",
+    "authDomain": "computervision-f3825.firebaseapp.com",
+    "databaseURL": "https://computervision-f3825-default-rtdb.firebaseio.com/",
+    "projectId": "computervision-f3825",
+    "storageBucket": "computervision-f3825.appspot.com",
+    "messagingSenderId": "811571419343",
+    "appId": "1:811571419343:web:a41770b10d5c521db7ffba",
+    "measurementId": "G-921FTZZHHR"
+}
+
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+
+fireFolderName = "FRDC"
+fireFileName = "2023-12-06_18:14.zip"
+
+# try:
+#     storage.child("FRDC/2023-12-06_18:14.zip").download("/store","file.zip")
+#     print(f"File '{fireFileName}' downloaded successfully")
+# except Exception as e:
+#     print(f"Error downloading file '{fireFileName}': {e}")
+
+
+def extract_zip(zip_file_path, extract_to_folder):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to_folder)
+
+def print_for_all_files(folder_path):
+    files = os.listdir(folder_path)
+    for file_name in files:
+        print(file_name)
+        print_all_digits(file_name)
+
+zip_file_path = 'file.zip'
+extract_to_folder = 'unzip'
+
+extract_zip(zip_file_path, extract_to_folder)
+print_for_all_files(extract_to_folder)
